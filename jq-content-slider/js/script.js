e494e5654fd12e3aed3abe4a08eeef1d70e3df89
rb <rb@email.com>
2015-11-22 19:27:24 -0800

    added jquery code. setting slide class with jquery instead of hard coding in html.

--- jq-content-slider/js/script.js
+++ jq-content-slider/js/script.js
@@ -3,5 +3,11 @@ any js code.*/
 
 $(document).ready(function() {
 	//alert("Your webpage is fully loaded!");
+	var sliding_speed = 500; /*The time (millisecond) taken to show the next slide*/
+	var autoswitch = true; /*If you want the slides to be changed automatically*/
+	var autoswitch_speed = 4000; /*milliseconds*/
+	
+	//Add the class 'active' to the first slide only.
+	$('.slide').first().addClass('active');
 }); 
 
