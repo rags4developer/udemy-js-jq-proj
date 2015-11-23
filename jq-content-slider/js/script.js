d31dd2af5c854400743d7b0ade2dfb6c4fc24fd2
rb <rb@email.com>
2015-11-23 14:12:39 -0800

    added detailed comments for next button press code. added code for prev button press. Slider is functioning!

--- jq-content-slider/js/script.js
+++ jq-content-slider/js/script.js
@@ -16,21 +16,65 @@ $(document).ready(function() {
 	//...show only the first slide
 	$('.active').show();
 	
+	//Code to execute when next button is pressed.
 	$('#next').on('click', function(){
-		$('.active').removeClass('active').addClass('oldActive'); /*'Hide the currently active slide and mark it as oldActive so that you can go back to it using the previous button.'*/
+		
+		/* Remove the active marking for current slide and mark the current slide as oldActive.
+		  oldActive will help us to access the next slide by referencing the currently displayed one.
+		*/
+		$('.active').removeClass('active').addClass('oldActive');
+			
+		/* Mark the following slide class "active. The following slide will be made current/visible 
+		in the next step*/
 		if($('.oldActive').is(':last-child')) {
 			$('.slide').first().addClass('active');
 		}else {
 			$('.oldActive').next().addClass('active');
 		}
+		
+		/*Remove the oldActive marker from current slide. The slide which you want to change to will become
+		the oldActive one when the button is pressed.
+		*/
 		$('.oldActive').removeClass('oldActive');
+		
+		/*Hide all slides and then display only the one we marked active*/
 		$('.slide').hide();
 		$('.active').show();
 		
-		//Animation
+		//Animation for changing slides.
 		$('.slide').fadeOut(sliding_speed);
 		$('.active').fadeIn(sliding_speed);
 	});
 	
-}); 
+	//Code to execute when previous button is pressed.	
+	$('#prev').on('click', function(){
+		
+		/* Remove the active marking for current slide and mark the current slide as oldActive.
+		  oldActive will help us to access the next slide by referencing the currently displayed one.
+		*/
+		$('.active').removeClass('active').addClass('oldActive');
+			
+		/* Mark the following slide class "active. The following slide will be made current/visible 
+		in the next step*/
+		if($('.oldActive').is(':first-child')) {
+			$('.slide').last().addClass('active');
+		}else {
+			$('.oldActive').prev().addClass('active');
+		}
+		
+		/*Remove the oldActive marker from current slide. The slide which you want to change to will become
+		the oldActive one when the button is pressed.
+		*/
+		$('.oldActive').removeClass('oldActive');
+		
+		/*Hide all slides and then display only the one we marked active*/
+		$('.slide').hide();
+		$('.active').show();
+		
+		//Animation for changing slides.
+		$('.slide').fadeOut(sliding_speed);
+		$('.active').fadeIn(sliding_speed);
+	});	
+		
+});
 
