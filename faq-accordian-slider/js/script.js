4a867180f48efa662e1a80cc9de4957731ded114
rb <rb@email.com>
2015-11-24 22:03:49 -0800

    added css class to rotate an image and also js to add/remove this class to a question.

--- faq-accordian-slider/js/script.js
+++ faq-accordian-slider/js/script.js
@@ -2,6 +2,7 @@ $('document').ready(function() {
 	$('.que').click(function() {
 		//Hide all answers except the one for clicked question.
 		$('.ans').not($(this).next()).hide();
+		$(this).children('img').toggleClass('rotate');//Rotate the image on click. 
 		$(this).next().toggle();
 	});
 });
